691a6f750f20c377a00f30c626c3bd1a817f831e
Nandan Kumar <nkumar101@sapient.com>
2018-08-13 07:53:00 +0530

    added basic functionality

--- src/Components/Boxes/Boxes.js
+++ src/Components/Boxes/Boxes.js
@@ -11,8 +11,8 @@ class Boxes extends Component {
   }
   render() {
     return (
-        <button className="square" onClick = {()=> this.setState({value : 'X'})} >
-            {this.state.value}
+        <button className="square" onClick = {()=> this.props.onClick()} >
+            {this.props.value}
         </button>
     );
   }
